1fc3115596ca2616c5890a3dc8b9ee66b13ab294
XiaohongXeLa2022 <97574185+xiaohong2022@users.noreply.github.com>
2022-08-14 10:32:54 +0800

    Update block.js

--- block.js
+++ block.js
@@ -103,7 +103,49 @@ Blockly.defineBlocksWithJsonArray([
     previousStatement: null,
     nextStatement: null,
     colour: 230,
-    tooltip: "",
+    tooltip: "设置地形上的方块",
+    helpUrl: "",
+  },
+  {
+    type: "voxels_get",
+    message0: "读取 x %1 y  z  的方块，返回方块ID",
+    args0: [
+      {
+        type: "input_value",
+        name: "X",
+      },
+    ],
+    output: "Number",
+    colour: 230,
+    tooltip: "读取地形上的方块",
+    helpUrl: "",
+  },
+  {
+    type: "voxels_name",
+    message0: "将方块ID %1 转换为名称",
+    args0: [
+      {
+        type: "input_value",
+        name: "ID",
+      },
+    ],
+    output: "String",
+    colour: 230,
+    tooltip: "将方块ID转换为方块名称",
+    helpUrl: "",
+  },
+  {
+    type: "voxels_id",
+    message0: "将方块名称 %1 转换为ID",
+    args0: [
+      {
+        type: "input_value",
+        name: "ID",
+      },
+    ],
+    output: "Number",
+    colour: 230,
+    tooltip: "将方块ID转换为方块名称",
     helpUrl: "",
   },
 ]);
@@ -159,4 +201,41 @@ Blockly.JavaScript["voxels_set"] = function (block) {
     Blockly.JavaScript.ORDER_ATOMIC
   );
   return "voxels.setVoxelId(" + x + "," + y + "," + z + "," + id + ");\n";
+};
+
+Blockly.JavaScript["voxels_get"] = function (block) {
+  var x = Blockly.JavaScript.valueToCode(
+    block,
+    "X",
+    Blockly.JavaScript.ORDER_ATOMIC
+  );
+  var y = Blockly.JavaScript.valueToCode(
+    block,
+    "Y",
+    Blockly.JavaScript.ORDER_ATOMIC
+  );
+  var z = Blockly.JavaScript.valueToCode(
+    block,
+    "Z",
+    Blockly.JavaScript.ORDER_ATOMIC
+  );
+  return "voxels.getVoxelId(" + x + "," + y + "," + z + ");\n";
+};
+
+Blockly.JavaScript["voxels_id"] = function (block) {
+  var id = Blockly.JavaScript.valueToCode(
+    block,
+    "ID",
+    Blockly.JavaScript.ORDER_ATOMIC
+  );
+  return "voxels.id(" + id + ");\n";
+};
+
+Blockly.JavaScript["voxels_name"] = function (block) {
+  var id = Blockly.JavaScript.valueToCode(
+    block,
+    "ID",
+    Blockly.JavaScript.ORDER_ATOMIC
+  );
+  return "voxels.name(" + id + ");\n";
 };
